bab3c7a751a9a02a72fad3a13d9f85b17ac4d04d
potmadu <potmadu@gmail.com>
2017-10-18 16:40:56 +0700

    Made general function and variables
    Made general function and variables

--- historical_tapinout_csv to bigdata.R
+++ historical_tapinout_csv to bigdata.R
@@ -1,49 +1,53 @@
-library(lubridate)
+library(lubridate);
+library(dplyr);
 
-tapin20170921 = read.csv("F:/tapin20170921.csv",stringsAsFactors=FALSE);
-tapin20170927 = read.csv("F:/tapin20170927.csv",stringsAsFactors=FALSE);
+tapinout = read.csv("F:/tapin20170921.csv",stringsAsFactors=FALSE);
 
-tapin20170921$tanggal=NULL;
-tapin20170927$tanggal=NULL;
-tapin20170921$jam=NULL;
-tapin20170927$jam=NULL;
-tapin20170921$tanggal_jam_upload=NULL;
-tapin20170927$tanggal_jam_upload=NULL;
+tapinout$tanggal=NULL;
+tapinout$jam=NULL;
+tapinout$tanggal_jam_upload=NULL;
+tapinout$koridor2 = gsub("\\]","",gsub("\\[","",gsub(" ","",substr(tapinout$koridor,1,4))));
+tapinout$koridor2[tapinout$koridor2=="N"]="NBRT";
+tapinout$koridor=tapinout$koridor2;
+tapinout$koridor2=NULL;
+tapinout$tanggal = dmy_hms(tapinout$tanggal_jam);
+tapinout$jam = hour(tapinout$tanggal);
 
-tapin20170921$koridor2 = gsub("\\]","",gsub("\\[","",gsub(" ","",substr(tapin20170921$koridor,1,4))));
-tapin20170927$koridor2 = gsub("\\]","",gsub("\\[","",gsub(" ","",substr(tapin20170927$koridor,1,4))));
+tapinout %>% 
+group_by(flag,jam) %>% 
+summarize(transaksi=n()) %>% 
+as.data.frame();
 
-tapin20170921$koridor2[tapin20170921$koridor2=="N"]="NBRT";
-tapin20170927$koridor2[tapin20170927$koridor2=="N"]="NBRT";
+tapinout$tanggal = strptime(tapinout$tanggal_jam,"%m-%d-%Y %H:%M:%S")
+tapinout$jam = strftime(tapinout$tanggal,"%H");
 
-tapin20170921$koridor=tapin20170921$koridor2;
-tapin20170927$koridor=tapin20170927$koridor2;
+tapinout_smy_tapin = tapinout %>% 
+select(-tanggal) %>% 
+filter(flag=="In") %>%
+group_by(flag,jam) %>% 
+summarize(transaksi=n()) %>% 
+as.data.frame();
 
-tapin20170921$koridor2=NULL;
-tapin20170927$koridor2=NULL;
+tapinout_smy_tapout = tapinout %>% 
+select(-tanggal) %>% 
+filter(flag=="Out") %>%
+group_by(flag,jam) %>% 
+summarize(transaksi=n()) %>% 
+as.data.frame();
 
-tapin20170921$tanggal = dmy_hms(tapin20170921$tanggal_jam);
-tapin20170921$jam = hour(tapin20170921$tanggal);
-tapin20170921 %>% group_by(flag,jam) %>% summarize(transaksi=n()) %>% as.data.frame();
+par(mfrow=c(1,2));
+plot(tapinout_smy_tapin$jam,tapin_smy_tapin$transaksi,type="l");
+plot(tapinout_smy_tapout$jam,tapin_smy_tapout$transaksi,type="l");
 
-write.csv(tapin20170921,"F:/tapin20170921",row.names=FALSE)
-write.csv(tapin20170927,"F:/tapin20170927",row.names=FALSE)
+write.csv(tapinout,"F:/tapin20170921.csv",row.names=FALSE)
 
-LOAD DATA LOCAL INPATH '/home/bigsql/staging/tapin20170921.csv' INTO TABLE stg.transj_tapinout_hist PARTITION (ppn_dttm=20170921);
-LOAD DATA LOCAL INPATH '/home/bigsql/staging/tapin20170927.csv' INTO TABLE stg.transj_tapinout_hist PARTITION (ppn_dttm=20170927);
+###################################################
 
-DATE_ID=20170921
-PPN_DTTM=20170925
+LOAD DATA LOCAL INPATH '/home/bigsql/staging/tapin20171012.csv' 
+INTO TABLE stg.transj_tapinout_hist PARTITION (ppn_dttm=20171012);
 
-hadoop fs -mkdir /apps/hive/warehouse/sor.db/transj_tapinout/ppn_dttm=${PPN_DTTM};
-hadoop fs -chmod 777 /apps/hive/warehouse/sor.db/transj_tapinout/ppn_dttm=${PPN_DTTM};
-
-hive -e "alter table SOR.TRANSJ_TAPINOUT DROP IF EXISTS PARTITION (date_id = '${DATE_ID}', ppn_dttm='${PPN_DTTM}');"
-hive -e "alter table SOR.TRANSJ_TAPINOUT add partition (date_id = '${DATE_ID}', ppn_dttm='${PPN_DTTM}') location '/apps/hive/warehouse/sor.db/transj_tapinout/ppn_dttm=${PPN_DTTM}';"
-hive -e "INSERT OVERWRITE TABLE SOR.TRANSJ_TAPINOUT PARTITION (date_id = ${DATE_ID}, ppn_dttm = ${PPN_DTTM}) select from_unixtime(unix_timestamp(TRANSJ_TAPINOUT_HIST.TANGGAL_JAM,'dd-MM-yyyy hh:mm:ss'),'dd-MM-yyyy') as TANGGAL_TRANSAKSI, from_unixtime(unix_timestamp(TRANSJ_TAPINOUT_HIST.TANGGAL_JAM,'dd-MM-yyyy hh:mm:ss'),'hh:mm:ss') as JAM_TRANSAKSI, TRANSJ_TAPINOUT_HIST.TANGGAL_JAM as TANGGAL_JAM_TRANSAKSI, TRANSJ_TAPINOUT_HIST.TANGGAL_JAM as TANGGAL_MASUK, PAN as NO_PAN, KARTU, KORIDOR, SUBKORIDOR as SUB_KORIDOR, HALTE, FLAG as FLAG_INOUT FROM stg.TRANSJ_TAPINOUT_HIST where PPN_DTTM = ${DATE_ID};"
-
-DATE_ID=20170927
-PPN_DTTM=20171001
+DATE_ID=20171012
+PPN_DTTM=20171016
 
 hadoop fs -mkdir /apps/hive/warehouse/sor.db/transj_tapinout/ppn_dttm=${PPN_DTTM};
 hadoop fs -chmod 777 /apps/hive/warehouse/sor.db/transj_tapinout/ppn_dttm=${PPN_DTTM};

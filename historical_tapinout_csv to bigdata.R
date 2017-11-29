17e67e81054aca4974230f57f963111d66eaf452
potmadu <potmadu@gmail.com>
2017-11-29 14:40:23 +0700

    update historical_tapinout_csv to bigdata code
    update historical_tapinout_csv to bigdata code

--- historical_tapinout_csv to bigdata.R
+++ historical_tapinout_csv to bigdata.R
@@ -5,18 +5,12 @@ library(dplyr);
 
 list=20170904,20170905,20171015,20171101,20171105,20171106,20171109
 
-tapinout = read.csv("20171109.csv",stringsAsFactors=FALSE);
+tanggal=20171116;
 
-tapinout$tanggal=NULL;
-tapinout$jam=NULL;
-tapinout$tanggal_jam_upload=NULL;
-tapinout$koridor2 = gsub("\\]","",gsub("\\[","",gsub(" ","",substr(tapinout$koridor,1,4))));
-tapinout$koridor2[tapinout$koridor2=="N"]="NBRT";
-tapinout$koridor=tapinout$koridor2;
-tapinout$koridor2=NULL;
+tapinout = read.csv(paste(tanggal,".csv",sep=""),stringsAsFactors=FALSE);
 
-write.csv(tapinout,"20171109_clean.csv",row.names=FALSE)
-
+###################################################
+# CEK DATA
 ###################################################
 
 tapinout$tanggal = dmy_hms(tapinout$tanggal_jam);
@@ -26,9 +20,6 @@ tapinout %>%
 group_by(flag,jam) %>% 
 summarize(transaksi=n()) %>% 
 as.data.frame();
-
-tapinout$tanggal = strptime(tapinout$tanggal_jam,"%m-%d-%Y %H:%M:%S")
-tapinout$jam = strftime(tapinout$tanggal,"%H");
 
 tapinout_smy_tapin = tapinout %>% 
 select(-tanggal) %>% 
@@ -45,10 +36,25 @@ summarize(transaksi=n()) %>%
 as.data.frame();
 
 par(mfrow=c(1,2));
-plot(tapinout_smy_tapin$jam,tapin_smy_tapin$transaksi,type="l");
-plot(tapinout_smy_tapout$jam,tapin_smy_tapout$transaksi,type="l");
+plot(tapinout_smy_tapin$jam,tapinout_smy_tapin$transaksi,type="l");
+plot(tapinout_smy_tapout$jam,tapinout_smy_tapout$transaksi,type="l");
 
-write.csv(tapinout,"F:/tapin20170921.csv",row.names=FALSE)
+tapinout$tanggal=NULL;
+tapinout$jam=NULL;
+
+###################################################
+# TRANSFORMASI DATA
+###################################################
+
+tapinout$tanggal=NULL;
+tapinout$jam=NULL;
+tapinout$tanggal_jam_upload=NULL;
+tapinout$koridor2 = gsub("\\]","",gsub("\\[","",gsub(" ","",substr(tapinout$koridor,1,4))));
+tapinout$koridor2[tapinout$koridor2=="N"]="NBRT";
+tapinout$koridor=tapinout$koridor2;
+tapinout$koridor2=NULL;
+
+write.csv(tapinout,paste(tanggal,"_clean.csv",sep=""),row.names=FALSE)
 
 ###################################################
 #ETL
@@ -56,12 +62,12 @@ write.csv(tapinout,"F:/tapin20170921.csv",row.names=FALSE)
 
 ## ON HIVE
 
-LOAD DATA LOCAL INPATH '/home/bigsql/staging/20170904_clean.csv' INTO TABLE stg.transj_tapinout_hist PARTITION (ppn_dttm=20170904);
+LOAD DATA LOCAL INPATH '/home/bigsql/staging/20171115_clean.csv' INTO TABLE stg.transj_tapinout_hist PARTITION (ppn_dttm=20171115);
 
 ## ON OS
 
-DATE_ID=20171101
-PPN_DTTM=20171105
+DATE_ID=20171116
+PPN_DTTM=20171120
 
 hadoop fs -mkdir /apps/hive/warehouse/sor.db/transj_tapinout/ppn_dttm=${PPN_DTTM};
 hadoop fs -chmod 777 /apps/hive/warehouse/sor.db/transj_tapinout/ppn_dttm=${PPN_DTTM};
